4f48aed49b51f6addd35d40824d47f5d521a489d
gonzo333 <pawelnowaczek88@gmail.com>
2017-01-04 19:49:05 +0100

    Białe znaki

--- server.js
+++ server.js
@@ -3,29 +3,29 @@ var app = express();
 
 app.use(express.static(__dirname + "/public"));
 
-app.get("/footballerList", function(req, res){
-	console.log("I receive a GET request");
+app.get("/footballerList", function(req, res) {
+    console.log("I receive a GET request");
 
-	footballer1 ={
-		name: "Robert Lewandowski",
-		club: "Bayern Munich",
-		notionality:"Poland"
-	};
+    footballer1 = {
+        name: "Robert Lewandowski",
+        club: "Bayern Munich",
+        notionality: "Poland"
+    };
 
-	footballer2 ={
-		name: "Cristiano Ronaldo",
-		club: "Real Madrit",
-		notionality:"Portugal"
-	};
+    footballer2 = {
+        name: "Cristiano Ronaldo",
+        club: "Real Madrit",
+        notionality: "Portugal"
+    };
 
-	footballer3 = {
-		name: "Leo Messi",
-		club: "Barcelona",
-		notionality:"Argentina"	
-	}
+    footballer3 = {
+        name: "Leo Messi",
+        club: "Barcelona",
+        notionality: "Argentina"
+    }
 
-	var footballerList = [footballer1, footballer2, footballer3];
-	res.json(footballerList);
+    var footballerList = [footballer1, footballer2, footballer3];
+    res.json(footballerList);
 
 });
 app.listen(3000)
